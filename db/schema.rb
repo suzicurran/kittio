31792a8793d4109fc1bdab5dfa5362fd343446e0
Suzi Curran <suzicurran@gmail.com>
2017-01-30 12:38:40 -0500

    create pets, add hound/rubocop config

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170130155127) do
+ActiveRecord::Schema.define(version: 20170130164037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.string   "name",                          null: false
+    t.string   "color",      default: "FFFFFF", null: false
+    t.integer  "user_id",                       null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.index ["user_id"], name: "index_pets_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
