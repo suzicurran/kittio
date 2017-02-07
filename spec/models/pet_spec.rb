c0accb14183087a1778805391657c07625bf8499
Suzi Curran <suzicurran@gmail.com>
2017-02-07 17:00:46 -0500

    now with passing tests

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Pet, type: :model do
-  describe "pet model" do
-    it "defaults color to white" do
+  describe "model" do
+    it "should have a name" do
       setup_pet = FactoryGirl.create(:pet)
-      expect(setup_pet.color).to eq("FFFFFF")
+      expect(setup_pet.name).to be_a(String)
     end
   end
 end
